b15d11381290be3c375673199660afb098f44a00
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-09-30 12:13:31 +0530

    commit

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NEXTJS 13 </h1>
-      <h2 className="h2-bold">NEXTJS 13 </h2>
-      <h3 className="h3-bold">NEXTJS 13 </h3>
-    </div>
-  );
-}
